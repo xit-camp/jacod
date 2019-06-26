6201270b02a7fae5b28cf135fd30bc9766952f49
Michal Hlavac <michal.hlavac@xit.camp>
2019-06-26 23:35:19 +0200

    use EntryMapper in ShallowRefProvider

--- core/src/main/java/camp/xit/jacod/impl/ShallowRefProvider.java
+++ core/src/main/java/camp/xit/jacod/impl/ShallowRefProvider.java
@@ -1,14 +1,15 @@
 package camp.xit.jacod.impl;
 
+import camp.xit.jacod.EntryMapper;
 import camp.xit.jacod.model.CodelistEntry;
 import camp.xit.jacod.provider.ReferenceProvider;
 
 public final class ShallowRefProvider implements ReferenceProvider {
 
-    private final CodelistEntryMapper mapper;
+    private final EntryMapper mapper;
 
 
-    public ShallowRefProvider(CodelistEntryMapper mapper) {
+    public ShallowRefProvider(EntryMapper mapper) {
         this.mapper = mapper;
     }
 
