472452d1f25c7d2513601e69fbb87554f85b84cf
Michal Hlavac <michal.hlavac@xit.camp>
2019-06-26 14:06:06 +0200

    remove unused javadoc

--- core/src/main/java/camp/xit/jacoa/BaseEntryMapping.java
+++ core/src/main/java/camp/xit/jacoa/BaseEntryMapping.java
@@ -6,10 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- *
- * @author hlavki
- */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BaseEntryMapping {
