29e6695e8456a2f323720794c8e45b0bfc2d88db
Michal Hlavac <michal.hlavac@xit.camp>
2022-05-30 22:14:13 +0200

    update CsvReader

--- core-spring-cache/src/test/java/camp/xit/jacod/spring/cache/test/CsvReader.java
+++ core-spring-cache/src/test/java/camp/xit/jacod/spring/cache/test/CsvReader.java
@@ -3,113 +3,63 @@ package camp.xit.jacod.spring.cache.test;
 import java.util.ArrayList;
 import java.util.List;
 
-class CsvReader {
+final class CsvReader {
 
     private static final char DEFAULT_SEPARATOR = ',';
-    private static final char DEFAULT_QUOTE = '"';
+    private static final char DOUBLE_QUOTES = '"';
+    private static final char DEFAULT_QUOTE_CHAR = DOUBLE_QUOTES;
 
+    private CsvReader() {}
 
-    private CsvReader() {
+    public static List<String> parseLine(String line) {
+        return parseLine(line, DEFAULT_SEPARATOR);
     }
 
-
-    public static List<String> parseLine(String cvsLine) {
-        return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
+    public static List<String> parseLine(String line, char separator) {
+        return parse(line, separator, DEFAULT_QUOTE_CHAR);
     }
 
-
-    public static List<String> parseLine(String cvsLine, char separators) {
-        return parseLine(cvsLine, separators, DEFAULT_QUOTE);
-    }
-
-
-    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
+    private static List<String> parse(String line, char separator, char quoteChar) {
 
         List<String> result = new ArrayList<>();
 
-        //if empty, return!
-        if (cvsLine == null && cvsLine.isEmpty()) {
-            return result;
-        }
-
-        if (customQuote == ' ') {
-            customQuote = DEFAULT_QUOTE;
-        }
-
-        if (separators == ' ') {
-            separators = DEFAULT_SEPARATOR;
-        }
-
-        StringBuffer curVal = new StringBuffer();
         boolean inQuotes = false;
-        boolean startCollectChar = false;
-        boolean doubleQuotesInColumn = false;
+        boolean isFieldWithEmbeddedDoubleQuotes = false;
 
-        char[] chars = cvsLine.toCharArray();
+        StringBuilder field = new StringBuilder();
 
-        for (char ch : chars) {
+        for (char c : line.toCharArray()) {
 
-            if (inQuotes) {
-                startCollectChar = true;
-                if (ch == customQuote) {
-                    inQuotes = false;
-                    doubleQuotesInColumn = false;
-                } else {
+            if (c == DOUBLE_QUOTES) { // handle embedded double quotes ""
+                if (isFieldWithEmbeddedDoubleQuotes) {
 
-                    //Fixed : allow "" in custom quote enclosed
-                    if (ch == '\"') {
-                        if (!doubleQuotesInColumn) {
-                            curVal.append(ch);
-                            doubleQuotesInColumn = true;
-                        }
-                    } else {
-                        curVal.append(ch);
+                    if (field.length() > 0) { // handle for empty field like "",""
+                        field.append(DOUBLE_QUOTES);
+                        isFieldWithEmbeddedDoubleQuotes = false;
                     }
 
+                } else {
+                    isFieldWithEmbeddedDoubleQuotes = true;
                 }
             } else {
-                if (ch == customQuote) {
-
-                    inQuotes = true;
-
-//                    //Fixed : allow "" in empty quote enclosed
-//                    if (chars[0] != '"' && customQuote == '\"') {
-//                        curVal.append('"');
-//                    }
-                    //double quotes in column will hit this!
-                    if (startCollectChar) {
-                        curVal.append('"');
-                    }
-
-                } else if (ch == separators) {
-
-                    result.add(curVal.toString());
-
-                    curVal = new StringBuffer();
-                    startCollectChar = false;
-
-                } else if (ch == '\r') {
-                    //ignore LF characters
-                    continue;
-                } else if (ch == '\n') {
-                    //the end, break!
-                    break;
-                } else {
-                    curVal.append(ch);
-                }
+                isFieldWithEmbeddedDoubleQuotes = false;
             }
 
+            if (c == quoteChar) {
+                inQuotes = !inQuotes;
+            } else {
+                if (c == separator && !inQuotes) { // if find separator and not in quotes, add field to the list
+                    result.add(field.toString());
+                    field.setLength(0); // empty the field and ready for the next
+                } else {
+                    field.append(c); // else append the char into a field
+                }
+            }
         }
 
-        result.add(curVal.toString());
+        // line done, what to do next?
+        result.add(field.toString()); // this is the last field
+
         return result;
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        String csv = "A,\"EMPLOYEE, TRADESMAN, STUDENT, UNEMPLOYED, MATERNITY_LEAVE, NANNY, CIVIL_SERVANT, SERVICE_PENSIONER, HOUSEPERSON, EMPLOYEE_ABROAD\"";
-
-        System.out.println(parseLine(csv).get(0));
-        System.out.println(parseLine(csv).get(1));
     }
 }
